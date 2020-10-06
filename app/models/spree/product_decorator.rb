a4e9a4d413da0c3e40875e9c4dfafc7f06479cf1
Hisham Malik <hisham@arkhitech.com>
2020-10-06 18:46:33 +0500

    updated for module prepend

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,8 +1,13 @@
-Spree::Product.class_eval do
-  has_one :slide, dependent: :destroy
-  after_update :destroy_slide_if_deleted
+module Spree
+  module ProductDecorator
+    def self.prepended(base)
+      base.has_one :slide, dependent: :destroy
+      base.after_update :destroy_slide_if_deleted  
+    end
 
-  def destroy_slide_if_deleted
-    slide.update_attributes(published: false) if slide && deleted_at
+    def destroy_slide_if_deleted
+      slide.update_attributes(published: false) if slide && deleted_at
+    end
+    Spree::Product.prepend self
   end
 end
